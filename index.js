64a78d86630e14b7f938a52b26c8cd5b46867f35
schasinghawk <schawk92@gmail.com>
2021-03-24 19:13:53 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+function add(number, b) {
+    return (number += b);
+}
+
+function subtract(number, b) {
+    return (number -= b);
+}
+
+function multiply(number, b) {
+    return (number *= b);
+}
+
+function divide(number, b) {
+    return (number /= b);
+}
+
+function increment(number) {
+    return (number + 1);
+}
+
+function decrement(number) {
+    return (number - 1);
+}
+
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string, 10);
+}
+
